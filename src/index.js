3d0ced707289d91c35c1f300eaec866a04aef45e
Yash Chaudhari <120511697+yashc2003@users.noreply.github.com>
2023-12-21 14:07:02 +0000

    setup file structare

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import ReactDOM from 'react-dom/client';
-import Home from './views/Home/index';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './views/Home/Home';
+import Products from './views/Products/Products';
+import Categories from './views/Categories/Categories';
+import Account from './views/Account/Account';
+import Cart from './views/Add Cart/Cart';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/products',
+    element: <Products />
+  },
+  {
+    path: '/categories',
+    element: <Categories />
+  },
+  {
+    path: '/account',
+    element: <Account />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<Home />);
+root.render(
+  <RouterProvider router={router}>
+    
+    <Home />
+    <Products />
+    <Categories />
+    <Account />
+    <Cart />
+  </RouterProvider>
+);
